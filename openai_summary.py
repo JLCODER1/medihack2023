23d4dd84879b4f1c2ca5fa1648506451184598a8
Jeff Lee <jl013awesome@gmail.com>
2023-09-10 14:10:38 -0700

    added comments

--- openai_summary.py
+++ openai_summary.py
@@ -2,7 +2,9 @@ import openai
 import run_google_search
 
 def summary(question: str) -> str:
+    #calls google search function
     link = run_google_search.search(question)
+    #gets info from website
     info = run_google_search.get_more_details(link)
 
     openai.api_key = '<Add api key here>'
@@ -10,14 +12,17 @@ def summary(question: str) -> str:
          model='gpt-3.5-turbo',
         messages= [{"role": "user", "content": info}],
         )
+    #previous code gives prompt to ai
     
+    #gets a summary/answer from ai
     summary = response.choices[0].message["content"]
     print(summary)
     return summary
 
 def main():
-    #summary(input())
-    summary('why is my poop black')
+    #prompt for ai
+    summary(input())
+    #summary('why is my poop black')
 
 if __name__ == '__main__':
     main()
